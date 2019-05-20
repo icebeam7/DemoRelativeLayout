71edce00edc410ea5a3d7939452ef6cf0ffa09e0
Luis Beltran <luis.beltran@itcelaya.edu.mx>
2019-05-21 01:51:48 +0200

    Add project files.

---
+++ DemoRelativeLayout/DemoRelativeLayout/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace DemoRelativeLayout
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var info = DeviceDisplay.MainDisplayInfo;
+            var width = info.Width;
+            var height = info.Height;
+
+            labelInfo.Text = $"Ancho: {width} | Altura: {height}";
+        }
+    }
+}
